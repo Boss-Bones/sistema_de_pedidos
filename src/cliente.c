6eeab929e5e19b23a195220ce7920d6283d9d123
shoriuguen <lucascosta7888@gmail.com>
2025-11-03 18:23:45 -0300

    structs basicas

--- src/cliente.c
+++ src/cliente.c
@@ -1,1 +1,31 @@
 #include "cliente.h"
+
+typedef struct cliente
+{
+   // Representa um cliente do sistema.
+
+    int id; // Um número de identificação único para cada cliente.
+    char nome[100]; // O nome do cliente.
+    char endereco[200]; // O endereço do cliente.
+    char telefone[20]; // O número de telefone do cliente.
+
+} cliente;
+
+typedef struct PessoaFisica
+{
+
+    // Representa um cliente pessoa física (CPF)
+
+    int id; // Mesmo número usado na struct cliente
+    char cpf[15]; // Char para incluir os digitos numéricos e especiais como o '-'
+
+} PessoaFisica;
+
+typedef struct PessoaJuridica
+{
+    // Representa um cliente pessoa jurídica (CNPJ)
+
+    int id; // Mesmo número usado na struct cliente
+    char cnpj[19]; // Char para incluir os digitos numéricos e especiais como o '-'
+
+} PessoaJuridica;
--- src/pedido.c
+++ src/pedido.c
@@ -1,1 +1,25 @@
 #include "pedido.h"
+
+typedef struct pedido
+{
+    // Representa uma ordem de compra feita por um cliente.
+
+    int id; // Um número de identificação único para cada pedido.
+    int clienteId; /* Armazena o id do cliente que fez o pedido, servindo como uma chave estrangeira
+    para a classe Cliente. */
+    char data[11]; // A data em que o pedido foi realizado (ex: "dd/mm/aaaa").
+    double total; // O valor total do pedido, que seria a soma dos subtotais de todos os itens.
+
+} pedido;
+
+typedef struct ItemPedido
+{
+    /* Esta é uma estrutura (struct) associativa. Ela representa um item específico dentro de um pedido, conectando
+    as estruturas Pedido e Produto. */
+
+     int pedidoId; // O id do pedido ao qual este item pertence.
+     int produtoId; // O id do produto que está sendo comprado.
+     int quantidade; // A quantidade daquele produto específico neste pedido.
+     double subtotal; // O valor total para este item (quantidade multiplicada pelo preco do Produto).
+
+} ItemPedido;
