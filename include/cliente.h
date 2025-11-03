6c6e5bc58e8bb2d6aeb9d51286ab248e7e7581d6
ClebioRibeiro <clebioribeiro89@gmail.com>
2025-11-03 17:56:42 -0300

    Começando escrever funçoes

--- include/cliente.h
+++ include/cliente.h
@@ -1,6 +1,16 @@
 #ifndef CLIENTE 
 #define CLIENTE
 
-// Coloque a prototipação da função aqui
+void inserirCliente();
+
+void listarCliente();
+
+void editarCliente();
+
+void removerCliente();
+
+void salvarClienteCSV();
+
+void carregarClienteCSV();
 
 #endif
--- src/cliente.c
+++ src/cliente.c
@@ -1,1 +1,13 @@
 #include "cliente.h"
+
+void inserirCliente(){}
+
+void listarCliente(){}
+
+void editarCliente(){}
+
+void removerCliente(){}
+
+void salvarClienteCSV(){}
+
+void carregarClienteCSV(){}
