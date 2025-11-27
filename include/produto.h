0b7733cb820a0a03e60e6f6ef7266df51526938b
shoriuguen <lucascosta7888@gmail.com>
2025-11-26 22:56:33 -0300

    Structs no .h

--- include/produto.h
+++ include/produto.h
@@ -1,6 +1,21 @@
 #ifndef PRODUTO 
 #define PRODUTO
 
+// ==== Estruturas ====
+
+typedef struct {
+    int id;
+    char descricao[100];
+    double preco;
+    int estoque;
+} Produto;
+
+typedef struct {
+    int quant; // Quantidade atual de produtos
+    int max; // Quantidade máxima a ser armazenada
+    Produto* produtos; // Ponteiro para structs de produto
+} ListaProduto;
+
 void inserirProduto();
 
 void listarProduto();
--- src/produto.c
+++ src/produto.c
@@ -3,21 +3,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-// ==== Estruturas ====
-
-typedef struct {
-    int id;
-    char descricao[100];
-    double preco;
-    int estoque;
-} Produto;
-
-typedef struct {
-    int quant; // Quantidade atual de produtos
-    int max; // Quantidade máxima a ser armazenada
-    Produto* produtos; // Ponteiro para structs de produto
-} ListaProduto;
-
 // ==== Funções ====
 
 Produto* consultarProduto(ListaProduto lprodutos, int id) {
