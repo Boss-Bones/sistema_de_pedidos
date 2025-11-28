aaf4e67f7c903c47528a3bc238ad5096b595a223
ClebioRibeiro <clebioribeiro89@gmail.com>
2025-11-28 19:56:53 -0300

    prototipação em persistencia.h

--- include/persistencia.h
+++ include/persistencia.h
@@ -2,8 +2,13 @@
 #define PERSISTENCIA
 
 // Coloque a prototipação da função aqui
-
+int salvarCliente(ListaCliente *clt, ListaCpf *cpf, ListaCnpj *cnpj);
+int carregarCliente(ListaCliente *clt, ListaCpf *cpf, ListaCnpj *cnpj);
 int salvarProduto(ListaProduto *pdt);
 int carregarProduto(ListaProduto *pdt);
+int salvarPedido(ListaPedido *pdd);
+int salvarItemPedido(ListaPedido *pdd);
+int carregarPedido(ListaPedido *pdd);
+int carregarItemPedido(ListaPedido *pdd);
 
 #endif
